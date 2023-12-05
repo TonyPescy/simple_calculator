d57217c8edd077ac40507d602dc67060bacc1700
TonyPescy <tpescy16@gmail.com>
2023-12-05 17:56:14 -0500

    Buttons made, simple file framework created

---
+++ README.md
@@ -0,0 +1,5 @@
+WIP
+
+A much simpler personal project that I can put less time in and get more done.
+
+Tony Pescatore
---
+++ simp_gui.py
@@ -0,0 +1,36 @@
+#######################################################################################################################################
+# Title: simp_gui
+# Date Started: 12/5/23
+# Date Completed: TBD
+# Author: Tony Pescatore
+# Description: Simple gui that will be used to build the calculators visuals
+#######################################################################################################################################
+
+# imports
+import PySimpleGUI as sg
+
+# buttons - all buttons will be created below
+# ALL BUTTON COLORS, FONT, AND SIZE ARE TBD ALONG WITH THEME PICKER OPTION
+
+# 0 button
+zero_but = sg.Button("0")
+# 1 button
+one_but = sg.Button("1")
+# 2 button
+two_but = sg.Button("2")
+# 3 button
+three_but = sg.Button("3")
+# 5 button
+four_but = sg.Button("4")
+# 5 button
+five_but = sg.Button("5")
+# 6 button
+six_but = sg.Button("6")
+# 7 button
+seven_but = sg.Button("7")
+# 8 button
+eight_but = sg.Button("8")
+# 9 button
+nine_but = sg.Button("9")
+
+# 
