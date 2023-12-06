260888995ab33f415d2a7bdef39b94f67d138c66
TonyPescy <tpescy16@gmail.com>
2023-12-06 11:56:43 -0500

    Frames and buttont sizing being tested

--- simp_gui.py
+++ simp_gui.py
@@ -9,56 +9,112 @@
 # imports
 import PySimpleGUI as sg
 
+# constants
+LENGTH = 300
+HEIGHT = 700
+BUT_SIZE_MODIFIER = .015
+
 # buttons - all buttons will be created below
 # ALL BUTTON COLORS, FONT, AND SIZE ARE TBD ALONG WITH THEME PICKER OPTION
 # SYMBOLS ÷, ←
 
 # 0 button
-zero_but = sg.Button("0")
+zero_but = sg.Button("0", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 1 button
-one_but = sg.Button("1")
+one_but = sg.Button("1", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 2 button
-two_but = sg.Button("2")
+two_but = sg.Button("2", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 3 button
-three_but = sg.Button("3")
+three_but = sg.Button("3", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 4 button
-four_but = sg.Button("4")
+four_but = sg.Button("4", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 5 button
-five_but = sg.Button("5")
+five_but = sg.Button("5", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 6 button
-six_but = sg.Button("6")
+six_but = sg.Button("6", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 7 button
-seven_but = sg.Button("7")
+seven_but = sg.Button("7", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 8 button
-eight_but = sg.Button("8")
+eight_but = sg.Button("8", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # 9 button
-nine_but = sg.Button("9")
+nine_but = sg.Button("9", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # add button
-add_but = sg.Button("+")
+add_but = sg.Button("+", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # sub button
-sub_but = sg.Button("-")
+sub_but = sg.Button("-", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # multiply button
-mul_but = sg.Button("x")
+mul_but = sg.Button("x", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # divide button
-div_but = sg.Button("÷")
+div_but = sg.Button("÷", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # neg button
-neg_but = sg.Button("+/-")
+neg_but = sg.Button("+/-", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # backspace button
-back_but = sg.Button("←")
+back_but = sg.Button("←", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # clear button
-clr_but = sg.Button("clr")
+clr_but = sg.Button("clr", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # answer button
-ans_but = sg.Button("ans")
+ans_but = sg.Button("ans", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # exponent button
-exp_but = sg.Button("^")
+exp_but = sg.Button("^", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # ( button
-l_par_but = sg.Button("(")
+l_par_but = sg.Button("(", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # ) button
-r_par_but = sg.Button(")")
+r_par_but = sg.Button(")", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # deciaml button
-dec_but = sg.Button(".")
+dec_but = sg.Button(".", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # themes button
-theme_but = sg.Button("Thm")
+theme_but = sg.Button("Thm", size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))
 # equal button
-equ_but = sg.Button("=", bind_return_key = True)    # when enter is pressed, this button will be pressed
+equ_but = sg.Button("=", bind_return_key = True, size = (int(BUT_SIZE_MODIFIER * LENGTH), int(BUT_SIZE_MODIFIER * LENGTH)))    # when enter is pressed, this button will be pressed
 
+# solo layouts
+
+# display layout
+
+# button layout
+but_lay = [ [theme_but, ans_but, clr_but, back_but],
+            [l_par_but, r_par_but, exp_but, div_but ],
+            [seven_but, eight_but, nine_but, mul_but],
+            [four_but, five_but, six_but, sub_but],
+            [one_but, two_but, three_but, add_but],
+            [neg_but, zero_but, dec_but, equ_but]
+    ]
+
+disp_lay = [[sg.Button("test")]
+
+    ]
+
+# frames
+
+# display frame
+disp_frame = sg.Frame("", disp_lay, size = (LENGTH, HEIGHT - (LENGTH + LENGTH)), element_justification = "Center")
+# button frame
+but_frame = sg.Frame("", but_lay, size = (LENGTH, (LENGTH + LENGTH)))
+
+# combined layout
+full_lay = [[disp_frame],
+            [but_frame]
+    ]
+
+# window = sg.Window("TEST", full_lay, size = (lENGTH, HEIGHT), use_custom_titlebar = True)
+
+def main():
+    window = sg.Window("TEST", full_lay, size = (LENGTH, HEIGHT), use_custom_titlebar = True)
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED:
+            window.close()
+            break
+        if event == 'Personal Information':
+            # Set layout to personal info layout or create whole new window
+            break
+        if event == 'Settings':
+            # Set layout to settings or create whole new window
+            break
+        if event == 'Logout':
+            # log user out of app
+            break
+
+    window.close()
+main()
